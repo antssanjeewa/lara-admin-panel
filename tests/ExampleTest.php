bede1294518a6c21ab7dfabd27feaaf13b0399d2
Sameera Sanjeewa <sameera.sanjeewa@elegantmedia.lk>
2024-08-20 10:40:48 +0530

    initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vendor\MyPackageName\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
